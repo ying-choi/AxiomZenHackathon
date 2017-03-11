7b7cb0233ca8e669b68e2cac9733e3c9aa30544a
Lisa <lisacat101@gmail.com>
2017-03-11 11:43:01 -0800

    inital commit

--- breatheasy-master/breathe/src/App.js
+++ breatheasy-master/breathe/src/App.js
@@ -6,22 +6,25 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+          <style>
+            @import url('https://fonts.googleapis.com/css?family=Vollkorn');
+          </style>
 
-      <div className="App-header">
-      <h2>Breathe Easy</h2>
-      </div>
+          <div className="App-header">
+            <h2>Breathe Easy</h2>
+          </div>
 
-      <div className="Text">
-      <ol>
-      <li> Take a slow breath in through the nose, breathing into your lower belly (for about 4 seconds)</li>
-      <li> Hold your breath for 1 or 2 seconds </li>
-      <li> Exhale slowly through the mouth (for about 4 seconds) </li>
-      <li> Wait a few seconds before taking another breath </li>
-      </ol>
-      </div>
+          <div className="Text">
+              <ol>
+                  <li> Take a slow breath in through the nose, breathing into your lower belly (for about 4 seconds)</li>
+                  <li> Hold your breath for 1 or 2 seconds </li>
+                  <li> Exhale slowly through the mouth (for about 4 seconds) </li>
+                  <li> Wait a few seconds before taking another breath </li>
+              </ol>
+          </div>
 
 
-      <div className="Breathe-button"></div>
+          <div className="Breathe-button"></div>
 
       </div>
     );
