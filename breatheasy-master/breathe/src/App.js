c8f02a702b198e07e57819ab06b44ab38fd3e460
Ying <ying-choi@users.noreply.github.com>
2017-03-11 13:46:57 -0800

    Button event

--- breatheasy-master/breathe/src/App.js
+++ breatheasy-master/breathe/src/App.js
@@ -18,10 +18,14 @@ class App extends Component {
       <li> Exhale slowly through the mouth (for about 4 seconds) </li>
       <li> Wait a few seconds before taking another breath </li>
       </ol>
+
+      <button className="toggle-button"><ToggleButton /></button>
+
+      <div className="Breathe-button-static"> </div>
+
       </div>
 
 
-      <div className="Breathe-button"></div>
 
       </div>
     );
@@ -29,3 +33,58 @@ class App extends Component {
 }
 
 export default App;
+
+// class Toggle extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {isToggleOn: true};
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+//
+//   handleClick() {
+//     this.setState(prevState => ({
+//       isToggleOn: !prevState.isToggleOn
+//     }));
+//   }
+//
+//   render() {
+//     return (
+//       <button onClick={this.handleClick}>
+//       {this.state.isToggleOn ? '.Start' : '.Stop'}
+//       </button>
+//     );
+//   }
+// }
+
+var ToggleButton = React.createClass({
+  getInitialState: function(){
+    return {
+      text: 'Stop'
+    };
+  },
+  handleClick: function(){
+    if (this.state.text === 'Stop'){
+      this.setState({text: 'Start'});
+    } else {
+      this.setState({text: 'Stop'});
+    }
+  },
+  render: function() {
+    return <div>
+    <button className={this.state.text} onClick={this.handleClick}>{this.state.text}</button>
+    {this.state.text === 'Start' && < Breathe />}
+    </div>
+  }
+
+})
+
+class Breathe extends React.Component {
+  constructor() {
+    super()
+  }
+  render(){
+    return(
+      <div className='Breathe-button-animate'></div>
+    )
+  }
+}
