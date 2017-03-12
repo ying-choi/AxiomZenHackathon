f8ecf61538e9750db1106f7a6b5d46dbe6102e31
Lisa <lisacat101@gmail.com>
2017-03-11 16:05:31 -0800

    WHAT IS THE POINT

--- breatheasy-master/breathe/src/App.js
+++ breatheasy-master/breathe/src/App.js
@@ -14,13 +14,14 @@ class App extends Component {
           </div>
 
           <div className="text">
+            <p className="intro">Often anxiety is caused by subconsciously hyperventilating. We are going to teach you how to breathe.</p>
+            <p className="intro">Click on the start button below to begin your journey</p>
               <ol>
                   <li> Take a slow breath in through the nose, breathing into your lower belly (for about <i>four seconds</i>)</li>
                   <li> Hold your breath for one or <i>two seconds</i> </li>
                   <li> Exhale slowly through the mouth (for about <i>four seconds</i>) </li>
                   <li> Wait a few seconds before taking another breath </li>
               </ol>
-              <p>Click the start button.</p>
           </div>
 
       <div className="toggle-button-class" ><ToggleButton /></div>
